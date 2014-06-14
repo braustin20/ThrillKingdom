a336d5eca3268aeb013a383aba961561cc669cb8
andrew911 <andrewrorer117@gmail.com>
2014-06-14 17:30:59 -0400

    Fixed UE4 boilerplate

--- Source/ThrillKingdom/Guest.h
+++ Source/ThrillKingdom/Guest.h
@@ -43,29 +43,29 @@ class AGuest : public ACharacter
 		 *	Honestly chances are that all of these values are going to have to be tweaked
 		 */
 
-		UPROPERTY()
+		UPROPERTY(Const, VisibleDefaultsOnly, Category = constants)
 			const float MIN_THIRST = 0.0f;
-		UPROPERTY()
+		UPROPERTY(Const, VisibleDefaultsOnly, Category = constants)
 			const float MAX_THIRST = 100.0f;
 
-		UPROPERTY()
+		UPROPERTY(Const, VisibleDefaultsOnly, Category = constants)
 			const float MIN_HUNGER = 0.0f;
-		UPROPERTY()
+		UPROPERTY(Const, VisibleDefaultsOnly, Category = constants)
 			const float MAX_HUNGER = 100.0f;
 
-		UPROPERTY()
+		UPROPERTY(Const, VisibleDefaultsOnly, Category = constants)
 			const float MIN_MOOD = -100.0f;
-		UPROPERTY()
+		UPROPERTY(Const, VisibleDefaultsOnly, Category = constants)
 			const float MAX_MOOD = 100.0f;
 
-		UPROPERTY()
+		UPROPERTY(Const, VisibleDefaultsOnly, Category = constants)
 			const float MIN_ENERGY = 0.0f;
-		UPROPERTY()
+		UPROPERTY(Const, VisibleDefaultsOnly, Category = constants)
 			const float MAX_ENERGY = 100.0f;
 
-		UPROPERTY()
+		UPROPERTY(Const, VisibleDefaultsOnly, Category = constants)
 			const float MIN_WALLET = 0.0f;
-		UPROPERTY()
+		UPROPERTY(Const, VisibleDefaultsOnly, Category = constants)
 			const float MAX_WALLET = 99999.99f;     //TODO: figure out a good value for this
 
 		/*
@@ -82,63 +82,51 @@ class AGuest : public ACharacter
 		//TODO: write a set of functions using NTCS instead of FStrings
 
 		//The guest's name
-		UPROPERTY()
-			FString GuestName;
+		FString GuestName;
 
 		//A string containing a short status update regarding the guest
-		UPROPERTY()
-			FString Status;
+		FString Status;
 
 		//A string containing the thoughts of the guest about the park.
 		//Not 100% sure if we need one of these, what do you guys think?
-		UPROPERTY()
-			FString CurrThought;
+		FString CurrThought;
 
 		//Represents how thirsty the guest is, always positive.
 		//0 meeans no thirst, 100.0f is the maximum value for thirst.
-		UPROPERTY()
-			float Thirst;
+		float Thirst;
 
 		//Represents how hungry the guest is, always positive.
 		//0 means no hunger, 100.0f is the maximum value for hunger.
-		UPROPERTY()
-			float Hunger;
+		float Hunger;
 
 		//Represents how the guest feels.  0 is neutral,
 		//a negative number represents a bad mood, a positive number
 		//represents a good mood. -100.0f and 100.0f are the lower
 		//and upper limits respectively.
-		UPROPERTY()
-			float Mood;
+		float Mood;
 
 		//Represents how tired the guest is, always positive.
 		//0 means no energy, 100.0f is the maximum amount of energy
-		UPROPERTY()
-			float Energy;
+		float Energy;
 
 		//Represents how sick the guest feels, always positive.
 		//0 means the guest does not feel sick, and 100.0f means the guest
 		//feels very sick
-		UPROPERTY()
-			float Nauseau;
+		float Nauseau;
 
-		//TODO: decide the upper limit on the guest wallet
 		//Represents the amount of money the guest has, is never
 		//a negative number.  Always truncated to two decimal
 		//points.
-		UPROPERTY()
-			float Wallet;
+		float Wallet;
 
 		//Represents the age of the guest.  Determined by the protected
 		//method generateAge when a Guest is initialized.
-		UPROPERTY()
-			int8 Age;
+		int8 Age;
 
 		//Represents the guest's Gender.  Determined by the protected
 		//method generateGender when a Guest is initialized, or when
 		//initialized through the constructor.
-		UPROPERTY()
-			EGuestGender::Gender Gender;
+		EGuestGender::Gender Gender;
 
 		//TODO: write container for guest inventory
 
@@ -158,11 +146,23 @@ class AGuest : public ACharacter
 		 *	All methods will need to be refactored at some point as well
 		 */
 
-		FString GetName();
-		FString GetStatus();
-		float GetWallet();
-		int GetAge();
-		EGuestGender::Gender GetGender();
+		UFUNCTION(BlueprintCallable, Category = getters)
+			FString GetName();
+
+		UFUNCTION(BlueprintCallable, Category = getters)
+			FString GetStatus();
+
+		UFUNCTION(BlueprintCallable, Category = getters)
+			FString GetCurrThought();
+
+		UFUNCTION(BlueprintCallable, Category = getters)
+			float GetWallet();
+
+		UFUNCTION(BlueprintCallable, Category = getters)
+			int GetAge();
+
+		UFUNCTION(BlueprintCallable, Category = getters)
+			EGuestGender::Gender GetGender();
 
 		/*
 		 *	End Getter methods
@@ -172,15 +172,27 @@ class AGuest : public ACharacter
 		 *	Start Setter methods
 		 */
 
-		void SetName(FString Name);
-		void SetStatus(FString NewStatus);
+		UFUNCTION(BlueprintCallable, Category = setters)
+			void SetName(FString Name);
+
+		UFUNCTION(BlueprintCallable, Category = setters)
+			void SetStatus(FString NewStatus);
 
 		//not sure if I want to keep any of the below setters, but I have a feeling they might be useful, delete them if they don't end up being used
-		void SetHunger(float NewHunger);
-		void SetThirst(float NewThirst);
-		void SetMood(float NewMood);
-		void SetEnergy(float NewEnergy);
-		void SetWallet(float NewWallet);
+		UFUNCTION(BlueprintCallable, Category = setters)
+			void SetHunger(float NewHunger);
+		
+		UFUNCTION(BlueprintCallable, Category = setters)
+			void SetThirst(float NewThirst);
+
+		UFUNCTION(BlueprintCallable, Category = setters)
+			void SetMood(float NewMood);
+
+		UFUNCTION(BlueprintCallable, Category = setters)
+			void SetEnergy(float NewEnergy);
+
+		UFUNCTION(BlueprintCallable, Category = setters)
+			void SetWallet(float NewWallet);
 
 		/*
 		 *	End Setter Methods
@@ -192,20 +204,30 @@ class AGuest : public ACharacter
 		 *	May want to use excpetions in these to detect errors, or return boolean values to designate failure or success
 		 */
 
-		void Spend(float Amount);
-		void Withdraw(float Amount);
+		UFUNCTION(BlueprintCallable, Category = actions)
+			void Spend(float Amount);
+		UFUNCTION(BlueprintCallable, Category = actions)
+			void Withdraw(float Amount);
 
-		void UpThirst(float Amount);
-		void DownThirst(float Amount);
+		UFUNCTION(BlueprintCallable, Category = actions)
+			void UpThirst(float Amount);
+		UFUNCTION(BlueprintCallable, Category = actions)
+			void DownThirst(float Amount);
 
-		void UpHunger(float Amount);
-		void DownHunger(float Amount);
+		UFUNCTION(BlueprintCallable, Category = actions)
+			void UpHunger(float Amount);
+		UFUNCTION(BlueprintCallable, Category = actions)
+			void DownHunger(float Amount);
 
-		void UpMood(float Amount);
-		void DownMood(float Amount);
+		UFUNCTION(BlueprintCallable, Category = actions)
+			void UpMood(float Amount);
+		UFUNCTION(BlueprintCallable, Category = actions)
+			void DownMood(float Amount);
 
-		void UpEnergy(float Amount);
-		void DownEnergy(float Amount);
+		UFUNCTION(BlueprintCallable, Category = actions)
+			void UpEnergy(float Amount);
+		UFUNCTION(BlueprintCallable, Category = actions)
+			void DownEnergy(float Amount);
 
 		virtual void Interact(AAttraction* CurrAttraction);
 
