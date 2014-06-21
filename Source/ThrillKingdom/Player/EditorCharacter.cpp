770ed310b6dad637a71fa860f7c993de6b2701bc
Tarwine <tarwine@live.com>
2014-06-21 18:01:29 -0400

    Setting up classes for camera

--- Source/ThrillKingdom/Player/EditorCharacter.cpp
+++ Source/ThrillKingdom/Player/EditorCharacter.cpp
@@ -7,7 +7,39 @@
 AEditorCharacter::AEditorCharacter(const class FPostConstructInitializeProperties& PCIP)
 	: Super(PCIP)
 {
+	// Structure to hold one-time initialization
+	struct FConstructorStatics
+	{
 
+	};
+
+	// Create camera component 
+	Camera = PCIP.CreateDefaultSubobject<UCameraComponent>(this, TEXT("Camera0"));
+	Camera->AttachTo(Base, USpringArmComponent::SocketName);
+	// Don't rotate camera with controller
+	Camera->bUseControllerViewRotation = false; 
 }
 
+// Frame loop
+void AEditorCharacter::Tick(float DeltaSeconds)
+{
+	// Call any parent class Tick implementation
+	Super::Tick(DeltaSeconds);
+}
 
+void AEditorCharacter::ReceiveHit(class UPrimitiveComponent* MyComp, class AActor* Other, class UPrimitiveComponent* OtherComp, bool bSelfMoved, FVector HitLocation, FVector HitNormal, FVector NormalImpulse, const FHitResult& Hit)
+{
+	Super::ReceiveHit(MyComp, Other, OtherComp, bSelfMoved, HitLocation, HitNormal, NormalImpulse, Hit);
+}
+
+void AEditorCharacter::SetupPlayerInputComponent(class UInputComponent* InputComponent)
+{
+	check(InputComponent);
+
+	InputComponent->BindAxis("Forward", this, &AEditorCharacter::MoveForward);
+
+}
+void AEditorCharacter::MoveForward(float Val)
+{
+
+}
--- Source/ThrillKingdom/Player/EditorCharacter.h
+++ Source/ThrillKingdom/Player/EditorCharacter.h
@@ -2,17 +2,38 @@
 
 #pragma once
 
-#include "GameFramework/Character.h"
+#include "GameFramework/Pawn.h"
 #include "EditorCharacter.generated.h"
 
 /**
  * 
  */
 UCLASS()
-class AEditorCharacter : public ACharacter
+class AEditorCharacter : public APawn
 {
+public:
+
 	GENERATED_UCLASS_BODY()
 
+	/** Component used as a base and collision */
+	UPROPERTY(Category = Default, VisibleDefaultsOnly, BlueprintReadOnly)
+	TSubobjectPtr<class UCapsuleComponent> Base;
+
+	/** Camera component that will be our viewpoint */
+	UPROPERTY(Category = Camera, VisibleDefaultsOnly, BlueprintReadOnly)
+	TSubobjectPtr<class UCameraComponent> Camera;
 	
-	
+	// Begin AActor overrides
+	virtual void Tick(float DeltaSeconds) OVERRIDE;
+	virtual void ReceiveHit(class UPrimitiveComponent* MyComp, class AActor* Other, class UPrimitiveComponent* OtherComp, bool bSelfMoved, FVector HitLocation, FVector HitNormal, FVector NormalImpulse, const FHitResult& Hit) OVERRIDE;
+	// End AActor overrides
+
+protected:
+
+	// Begin APawn overrides
+	virtual void SetupPlayerInputComponent(class UInputComponent* InputComponent) OVERRIDE; // Allows binding actions/axes to functions
+	// End APawn overrides
+
+	// Called when forward input is pressed
+	void MoveForward(float Val);
 };
