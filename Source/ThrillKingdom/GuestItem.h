0083a1a906d9ac14a5f671ece1f9052555a32389
andrew911 <andrewrorer117@gmail.com>
2014-06-22 18:12:47 -0400

    Added EItemType and getters

--- Source/ThrillKingdom/GuestItem.h
+++ Source/ThrillKingdom/GuestItem.h
@@ -5,14 +5,50 @@
 #include "Object.h"
 #include "GuestItem.generated.h"
 
+
 /**
- * 
+
+*/
+UENUM(BlueprintType)
+namespace EItemType
+{
+	enum Type
+	{
+		food		UMETA(DisplayName = "food"),
+		drink		UMETA(DisplayName = "drink"),
+		souvenier	UMETA(DisplayName = "souvenier"),	
+		accesory	UMETA(DisplayName = "accesory"),
+		clothing	UMETA(DisplayName = "clothing"),
+		camera		UMETA(DisplayName = "camera")		//Not sure if this should be an item or just a bool in Guest like hasCamera
+	};
+}
+
+
+/**
+ 
  */
 UCLASS()
 class UGuestItem : public UObject
 {
+public:
 	GENERATED_UCLASS_BODY()
 
+
+	/*
+	 *	Start Getters
+	 */
+
+	FString GetName();
+	float GetPrice();
+	EItemType::Type GetType();
+
+	/*
+	 *	End Getters
+	 */
+
+
+private:
 	FString ItemName;
 	float Price;
+	EItemType::Type Type;
 };
