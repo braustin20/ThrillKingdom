4226b20874adf79466e3ac3327af82c24937005a
andrew911 <andrewrorer117@gmail.com>
2014-06-25 01:23:19 -0400

    Scrapping the last few changes, trying a different approach

--- Source/ThrillKingdom/GameModes/ParkGameMode.cpp
+++ Source/ThrillKingdom/GameModes/ParkGameMode.cpp
@@ -11,33 +11,7 @@
 AParkGameMode::AParkGameMode(const class FPostConstructInitializeProperties& PCIP)
 	: Super(PCIP)
 {
-	std::string CLine;
-	//FString UELine;
-
-	//std::ifstream MaleNameIn("maleNamesFormatted.txt");
-	//std::ifstream FemNameIn("femaleNamesFormatted.txt");
-	//std::ifstream LastNameIn("lastNamesFormatted.txt");
-
-	//for (int16 i = 0; i < 1062; i++)
-	//{
-	//	std::getline(MaleNameIn, CLine);
-	//	UELine = CLine.c_str();
-	//	MaleName::FirstName[i] = UELine;
-	//}
-
-	//for (int16 i = 0; i < 4076; i++)
-	//{
-	//	std::getline(FemNameIn, CLine);
-	//	UELine = CLine.c_str();
-	//	FemaleName::FirstName[i] = UELine;
-	//}
-
-	//for (int16 i = 0; i < 54010; i++)
-	//{
-	//	std::getline(LastNameIn, CLine);
-	//	UELine = CLine.c_str();
-	//	LastName::LastName[i] = UELine;
-	//}
+	
 }
 
 
