59fccfa0412ef44ebeca69f9429ec925b357c5f1
andrew911 <andrewrorer117@gmail.com>
2014-06-25 03:03:44 -0400

    Whoops, included the wrong file in that last commit.  Just replaced it

---
+++ Source/ThrillKingdom/GuestNameData.h
@@ -0,0 +1,17 @@
+
+
+#pragma once
+
+#include "Runtime/Engine/Classes/Engine/DataTable.h"
+
+/**
+ *	
+ */
+USTRUCT(BlueprintType)
+class FGuestNameData : public FTableRowBase
+{
+	GENERATED_USTRUCT_BODY()
+public:
+
+	
+};
