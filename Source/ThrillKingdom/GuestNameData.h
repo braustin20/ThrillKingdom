2fc9468564dff60c91a9077b0be5cd37501fb88d
andrew911 <andrewrorer117@gmail.com>
2014-06-25 04:24:42 -0400

    Finished GuestNameData.h and imported the CSVs

--- Source/ThrillKingdom/GuestNameData.h
+++ Source/ThrillKingdom/GuestNameData.h
@@ -3,15 +3,30 @@
 #pragma once
 
 #include "Runtime/Engine/Classes/Engine/DataTable.h"
+#include "Runtime/Core/Public/HAL/Platform.h"
+#include "GuestNameData.generated.h"
 
 /**
  *	
  */
 USTRUCT(BlueprintType)
-class FGuestNameData : public FTableRowBase
+struct FGuestNameData : public FTableRowBase
 {
 	GENERATED_USTRUCT_BODY()
 public:
 
+	FGuestNameData()
+		: Frequency(0)
+		, ApproxNumInUSA(0)
+		, Ranking(0)
+	{}
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = GuestNameData)
+		float Frequency;
 	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = GuestNameData)
+		int32 ApproxNumInUSA;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = GuestNameData)
+		int32 Ranking;
 };
