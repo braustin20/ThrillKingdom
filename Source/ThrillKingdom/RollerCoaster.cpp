f780e0a6a2dcf833eeae2ce254b9e99bc269eb57
andrew911 <andrewrorer117@gmail.com>
2014-07-04 02:34:52 -0400

    Added source and header files for RollerCoaster class

---
+++ Source/ThrillKingdom/RollerCoaster.cpp
@@ -0,0 +1,13 @@
+
+
+#include "ThrillKingdom.h"
+#include "RollerCoaster.h"
+
+
+ARollerCoaster::ARollerCoaster(const class FPostConstructInitializeProperties& PCIP)
+	: Super(PCIP)
+{
+
+}
+
+
---
+++ Source/ThrillKingdom/RollerCoaster.h
@@ -0,0 +1,18 @@
+
+
+#pragma once
+
+#include "Ride.h"
+#include "RollerCoaster.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARollerCoaster : public ARide
+{
+	GENERATED_UCLASS_BODY()
+
+	
+	
+};
