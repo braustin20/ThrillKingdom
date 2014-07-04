903178d45410cf3b1ebd5c24aa897fbb7ecde0ac
andrew911 <andrewrorer117@gmail.com>
2014-07-03 21:35:40 -0400

    Added contants to Ride.h and implimented them

--- Source/ThrillKingdom/Ride.cpp
+++ Source/ThrillKingdom/Ride.cpp
@@ -7,7 +7,20 @@
 ARide::ARide(const class FPostConstructInitializeProperties& PCIP)
 	: Super(PCIP)
 {
+	MinDurability = 0.0f;
+	MaxDurability = 100.0f;
 
+	MinThrillFactor = 0.0f;
+	MaxThrillFactor = 10.0f;
+
+	MinEnjoymentFactor = 0.0f;
+	MaxEnjoymentFactor = 10.0f;
+
+	MinNauseaFactor = 0.0f;
+	MaxNauseaFactor = 10.0f;
+
+	MinGForceFactor = 0.0f;
+	MaxGForceFactor = 10.0f;
 }
 
 
--- Source/ThrillKingdom/Ride.h
+++ Source/ThrillKingdom/Ride.h
@@ -16,23 +16,66 @@ class ARide : public AAttraction
 	GENERATED_UCLASS_BODY()
 public:
 
+	/*
+	 *	Start Getters
+	 */
+
 	float GetDurability();
 	float GetThrillFactor();
 	float GetEnjoymentFactor();
 	float GetNauseaFactor();
 	float GetGForceFactor();
 
+	/*
+	 *	End Getters
+	 */
+
+
+	/*
+	 *	Start Setters
+	 */
+
 	void SetDurability(float NewDurability);
+
+	/*
+	 *	End Setters
+	 */
 
 protected:
 
 	/*
 	 *  Start Fields
 	 */
-
+	
+	//TODO: work on finding good types and values for these fields
 	float Durability;
 	float ThrillFactor;
 	float EnjoymentFactor;
 	float NauseaFactor;
 	float GForceFactor;
+
+	/*
+	 *	End Fields
+	 */
+
+	
+	/*
+	 *	Start Constants
+	 */
+
+	//TODO: make these actual constants when their associated variables are their final types and values
+	float MinDurability;
+	float MaxDurability;
+
+	float MinThrillFactor;
+	float MaxThrillFactor;
+
+	float MinEnjoymentFactor;
+	float MaxEnjoymentFactor;
+
+	float MinNauseaFactor;
+	float MaxNauseaFactor;
+
+	float MinGForceFactor;
+	float MaxGForceFactor;
 };
